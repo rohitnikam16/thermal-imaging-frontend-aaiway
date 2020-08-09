4059b95bd1c2abaa6d34f9c5c8cab8df11ac3aea
Anuragtech02 <apal895@gmail.com>
2020-08-09 18:34:51 +0530

    Updates

---
+++ precache-manifest.68098cad7064857e5b250ef1ae65aad2.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37d78f50ca5702dd03ac0315b0ace634",
+    "url": "/thermal-imaging-frotnend/index.html"
+  },
+  {
+    "revision": "91767cf783d6dddd7eaf",
+    "url": "/thermal-imaging-frotnend/static/css/main.2de6d15c.chunk.css"
+  },
+  {
+    "revision": "7bbf80c3d36277b31345",
+    "url": "/thermal-imaging-frotnend/static/js/2.96b9f4fa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/thermal-imaging-frotnend/static/js/2.96b9f4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91767cf783d6dddd7eaf",
+    "url": "/thermal-imaging-frotnend/static/js/main.90d820ce.chunk.js"
+  },
+  {
+    "revision": "25d2d5eefa4ea7d18af1",
+    "url": "/thermal-imaging-frotnend/static/js/runtime-main.8a532e09.js"
+  },
+  {
+    "revision": "970d73b7a8642d56fad254f85ab41721",
+    "url": "/thermal-imaging-frotnend/static/media/heroLogin.970d73b7.svg"
+  },
+  {
+    "revision": "2f55413421f215164ab2fbad5c61212b",
+    "url": "/thermal-imaging-frotnend/static/media/video.2f554134.webm"
+  }
+]);

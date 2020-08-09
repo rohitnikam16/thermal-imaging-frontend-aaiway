9cd85e40b5fbc7b600c0c776330d352dadc9efa9
Anuragtech02 <apal895@gmail.com>
2020-08-09 18:37:57 +0530

    Updates

---
+++ precache-manifest.158fcd9690e329e906505bae0502150a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63edd0790b6a8e260b75d7817d03d903",
+    "url": "/thermal-imaging-frontend-aaiway/index.html"
+  },
+  {
+    "revision": "91767cf783d6dddd7eaf",
+    "url": "/thermal-imaging-frontend-aaiway/static/css/main.2de6d15c.chunk.css"
+  },
+  {
+    "revision": "7bbf80c3d36277b31345",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/2.96b9f4fa.chunk.js"
+  },
+  {
+    "revision": "5e9b6f5c16b720e8a86875f97734673e",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/2.96b9f4fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "91767cf783d6dddd7eaf",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/main.90d820ce.chunk.js"
+  },
+  {
+    "revision": "d0a9acd74b7cb0eb4bad",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/runtime-main.38dfbae8.js"
+  },
+  {
+    "revision": "970d73b7a8642d56fad254f85ab41721",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/heroLogin.970d73b7.svg"
+  },
+  {
+    "revision": "2f55413421f215164ab2fbad5c61212b",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/video.2f554134.webm"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/thermal-imaging-frotnend/precache-manifest.68098cad7064857e5b250ef1ae65aad2.js"
+  "/thermal-imaging-frontend-aaiway/precache-manifest.158fcd9690e329e906505bae0502150a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thermal-imaging-frotnend/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thermal-imaging-frontend-aaiway/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });

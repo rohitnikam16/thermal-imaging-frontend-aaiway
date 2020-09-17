2c71e3d84ed53eef598bc5918bb45b19768d807d
Anuragtech02 <apal895@gmail.com>
2020-09-17 18:36:40 +0530

    Updates

---
+++ precache-manifest.153a912bea4a6288f2042ec2d71398d7.js
@@ -0,0 +1,90 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbf8163e3f4155a827ecdcee5d49d370",
+    "url": "/thermal-imaging-frontend-aaiway/index.html"
+  },
+  {
+    "revision": "79fd719f33fd4f72bd34",
+    "url": "/thermal-imaging-frontend-aaiway/static/css/main.1ffb855f.chunk.css"
+  },
+  {
+    "revision": "4eaa2460b8c479e72d70",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/2.ac2d6308.chunk.js"
+  },
+  {
+    "revision": "02b4d0f8c196549c8bdb280ef86031ad",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/2.ac2d6308.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79fd719f33fd4f72bd34",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/main.afc5aef1.chunk.js"
+  },
+  {
+    "revision": "d0a9acd74b7cb0eb4bad",
+    "url": "/thermal-imaging-frontend-aaiway/static/js/runtime-main.38dfbae8.js"
+  },
+  {
+    "revision": "03d89df8b6725e88e60d26626232d8fe",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/attendance.03d89df8.svg"
+  },
+  {
+    "revision": "fa7fdc80db4dd2e91e421265e440a9fa",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/camera.fa7fdc80.svg"
+  },
+  {
+    "revision": "363861ec99067ac99a7d231372d260ef",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/correct.363861ec.svg"
+  },
+  {
+    "revision": "9992ee62b8020f843374a5e95b045186",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/down-arrow.9992ee62.svg"
+  },
+  {
+    "revision": "c44fbb40054e312e4e9c0133c11f9e3d",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/face1.c44fbb40.jpg"
+  },
+  {
+    "revision": "a495cff300b8c82dbc4f1d0b69acdebc",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/face2.a495cff3.jpg"
+  },
+  {
+    "revision": "970d73b7a8642d56fad254f85ab41721",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/heroLogin.970d73b7.svg"
+  },
+  {
+    "revision": "8672af353e46b915f0130fef4e9c1865",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/mask.8672af35.svg"
+  },
+  {
+    "revision": "b74017a51e5f3c738a34efd1c46bb1cf",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/picture-thumbnail.b74017a5.svg"
+  },
+  {
+    "revision": "cf4c1f7f65c0e9fbb22e0b4877a30e6d",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/sunglasses.cf4c1f7f.svg"
+  },
+  {
+    "revision": "b920cca4ce046f15bc961fd21c55788c",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/therm1.b920cca4.jpg"
+  },
+  {
+    "revision": "f56c0522de5e279ea005b1aeb2fb2750",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/thermo.f56c0522.svg"
+  },
+  {
+    "revision": "250ee3689963328e177ce9f1ac3598ab",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/up-arrow.250ee368.svg"
+  },
+  {
+    "revision": "2f55413421f215164ab2fbad5c61212b",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/video.2f554134.webm"
+  },
+  {
+    "revision": "cc66763806ec0df081851f3c427812f5",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/visual1.cc667638.jpg"
+  },
+  {
+    "revision": "47009b7f91da019b1fc62e9dd8c2afcc",
+    "url": "/thermal-imaging-frontend-aaiway/static/media/wrong.47009b7f.svg"
+  }
+]);
